b6e0bd3a875c52324161bf05cf75be2c3feb9e39
moekaraki <mkaraki@gmail.com>
2023-11-17 18:10:25 -0800

    Created UserRepository in order for my application to keep my API neat and easily access data from multiple sources (UserDAO and network).

--- app/src/main/java/com/example/vision/UserRepository.java
+++ app/src/main/java/com/example/vision/UserRepository.java
@@ -1,4 +1,30 @@
 package com.example.vision;
 
+import android.app.Application;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class UserRepository {
+    private UserDao mUserDao;
+    private LiveData<List<User>> mAllUsers;
+    private static final int NUMBER_OF_THREADS = 4;
+    private static final ExecutorService databaseWriteExecuter = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+
+    UserRepository(Application application) {
+        UserRoomDatabase db = UserRoomDatabase.getDatabase(application);
+        mUserDao = db.userDao();
+        mAllUsers = mUserDao.getAllUsers();
+    }
+
+    LiveData<List<User>> getAllUsers() {
+        return mAllUsers;
+    }
+
+    void insert(User user) {
+        databaseWriteExecuter.execute(() -> { mUserDao.insert(user);
+        });
+    }
 }
